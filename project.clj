6eeabc2f8155e800cf9eb497b9a9676b2eccd158
Tomas Drencak <tomas@drencak.com>
2018-06-01 14:21:40 +0200

    Version 0.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject statecharts "0.2.3"
+(defproject statecharts "0.2.4-SNAPSHOT"
   :description "Statecharts for clojurescript and re-frame"
   :url "https://github.com/tomasd/statecharts"
   :license {:name "Eclipse Public License"
